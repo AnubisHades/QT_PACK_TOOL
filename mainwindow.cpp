ae2bd4bf49402a4e3ad718bb074261598b97a19f
Q <Q>
2022-04-17 17:32:21 +0800

    添加打包相关信息

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,6 +14,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->lineEdit->setText(tr("ping www.baidu.com"));
     ui->textEdit->setLineWrapMode(QTextEdit::NoWrap);
 
+    LoaderCmdArg();
+
     /* 新建process */
     process = new QProcess(this);
     process->setProcessChannelMode(QProcess::MergedChannels);
@@ -25,20 +27,30 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton_Enter->setShortcut(Qt::Key_Return);
 
     /* 按键相关 */
-    connect(ui->pushButton,SIGNAL(clicked()),ui->lineEdit,SLOT(clear()));                           //清除命令行
-    connect(ui->pushButton_Enter,SIGNAL(clicked()),this,SLOT(write_cmd()));                         //回车按钮
-    connect(ui->pushButton_Clear,SIGNAL(clicked()),ui->textEdit,SLOT(clear()));                     //清除文本框按钮
+    connect(ui->pushButton,SIGNAL(clicked()),
+            ui->lineEdit,SLOT(clear()));                           //清除命令行
+    connect(ui->pushButton_Enter,SIGNAL(clicked()),
+            this,SLOT(write_cmd()));                         //回车按钮
+    connect(ui->pushButton_Clear,SIGNAL(clicked()),
+            ui->textEdit,SLOT(clear()));                     //清除文本框按钮
 
     /* 命令行相关 */
-    connect(process,SIGNAL(readyRead()),this,SLOT(read_data()));                                    //读命令行数据
-    connect(process,SIGNAL(readyReadStandardOutput()),this,SLOT(read_data()));                      //读命令行标准数据（兼容）
-    connect(process,SIGNAL(errorOccurred(QProcess::ProcessError)),this,SLOT(error_process()));      //命令行错误处理
-    connect(process,SIGNAL(finished(int)),this,SLOT(finished_process()));                           //命令行结束处理
+    connect(process,SIGNAL(readyRead()),
+            this,SLOT(read_data()));                                    //读命令行数据
+    connect(process,SIGNAL(readyReadStandardOutput()),
+            this,SLOT(read_data()));                      //读命令行标准数据（兼容）
+    connect(process,SIGNAL(errorOccurred(QProcess::ProcessError)),
+            this,SLOT(error_process()));      //命令行错误处理
+    connect(process,SIGNAL(finished(int)),
+            this,SLOT(finished_process()));                           //命令行结束处理
 
     /* 焦点相关 */
-    connect(ui->pushButton,SIGNAL(clicked()),ui->lineEdit,SLOT(setFocus()));
-    connect(ui->pushButton_Enter,SIGNAL(clicked()),ui->lineEdit,SLOT(setFocus()));
-    connect(ui->pushButton_Clear,SIGNAL(clicked()),ui->lineEdit,SLOT(setFocus()));
+    connect(ui->pushButton,SIGNAL(clicked()),
+            ui->lineEdit,SLOT(setFocus()));
+    connect(ui->pushButton_Enter,SIGNAL(clicked()),
+            ui->lineEdit,SLOT(setFocus()));
+    connect(ui->pushButton_Clear,SIGNAL(clicked()),
+            ui->lineEdit,SLOT(setFocus()));
 
     process->start("cmd.exe");
 }
@@ -62,19 +74,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::write_cmd()
 {
-    /* 获得命令，\r\n代替键盘的Enter，没有则不会执行 */
-    QString cmd = ui->lineEdit->text() + "\r\n";
-    //ui->lineEdit->clear();
+    CmdWrite(ui->lineEdit->text());
     ui->lineEdit->setFocus();
-
-    /* 转为char*并写入 */
-    QByteArray qbarr = cmd.toLatin1();
-    char *ch = qbarr.data();
-    qint64 len = cmd.length();
-    process->write(ch, len);
-
-    /* 信息输出 */
-    qDebug() << "Success:write_cmd：" << cmd << endl;
 }
 
 void MainWindow::read_data()
@@ -115,7 +116,44 @@ void MainWindow::error_process()
     qDebug() << "Success:error_process():" << err << endl;
 }
 
+void MainWindow::on_pushButton_OpenFile_clicked()
+{
+    //打开待打包目录
+    FilePath = QFileDialog::getOpenFileName(this,
+                 tr("打开待打包文件"),
+                 "D:/",
+                 tr("所有文件(*.*);;待打包文件(*.exe)"));
+    //显示文件信息
+    ui->lineEdit_FilePath->setText(FilePath);
 
+    //打开文件
+    QFile File(FilePath);
+    File.open(QIODevice::ReadWrite | QIODevice::Text);
+
+    //获取文件信息
+    QFileInfo FileIno(File);
+    QString name = FileIno.fileName();
+
+    QDateTime current_time = QDateTime::currentDateTime();
+    QString currentTime = current_time.toString("yyyyMMdd_hhmm_ss");
+    currentTime = "Pack"+currentTime;
+    currentTime = "D:/"+currentTime;
+
+    //创建目录
+    QDir dir(NewFilePath); // 注意
+    if(dir.exists())
+    {
+        qDebug()<<"路径已存在";
+        return;
+    }else{
+        bool ok = dir.mkpath(NewFilePath);
+//        return ok;
+    }
+
+    NewFilePath = NewFilePath+"/"+name;
+    File.copy(NewFilePath);
+    File.close();
+}
 
 
 
@@ -217,3 +255,115 @@ void MainWindow::finished_process()
 }
 #endif
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+
+void MainWindow::on_lineEdit_2_textEdited(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
+{
+//    LoaderCmdArg();
+}
+void MainWindow::LoaderCmdArg()
+{
+    CmdArg  ="set PATH="
+            +ui->lineEdit_EnvPath1->text()
+            +";"
+            +ui->lineEdit_EnvPath2->text()
+            +";%PATH%";
+    qDebug()<<ui->lineEdit_EnvPath1->text();
+    qDebug()<<ui->lineEdit_EnvPath2->text();
+    qDebug()<<CmdArg;
+}
+void MainWindow::CmdWrite( QString CmdWrVal)
+{
+    /* 获得命令，\r\n代替键盘的Enter，没有则不会执行 */
+    QString cmd = CmdWrVal + "\r\n";
+
+    /* 转为char*并写入 */
+    QByteArray qbarr = cmd.toLatin1();
+    char *ch = qbarr.data();
+    qint64 len = cmd.length();
+    process->write(ch, len);
+
+    /* 信息输出 */
+    qDebug() << "Success:write_cmd：" << cmd << endl;
+}
+
+
+void MainWindow::on_pushButton_OpenFile_clicked()
+{
+    //打开待打包目录
+    FilePath = QFileDialog::getOpenFileName(this,
+                 tr("打开待打包文件"),
+                 "D:/",
+                 tr("所有文件(*.*);;待打包文件(*.exe)"));
+    //显示文件信息
+    ui->lineEdit_FilePath->setText(FilePath);
+
+    //打开文件
+    QFile File(FilePath);
+    File.open(QIODevice::ReadWrite | QIODevice::Text);
+
+    //获取文件信息
+    QFileInfo FileIno(File);
+    QString name = FileIno.fileName();
+
+    QDateTime current_time = QDateTime::currentDateTime();
+    QString currentTime = current_time.toString("yyyyMMdd_hhmm_ss");
+    currentTime = "Pack"+currentTime;
+    currentTime = "D:/"+currentTime;
+
+    //创建目录
+    QDir dir(NewFilePath); // 注意
+    if(dir.exists())
+    {
+        qDebug()<<"路径已存在";
+        return;
+    }else{
+        bool ok = dir.mkpath(NewFilePath);
+//        return ok;
+    }
+
+    NewFilePath = NewFilePath+"/"+name;
+    File.copy(NewFilePath);
+    File.close();
+}
+
+void MainWindow::on_pushButton_Pack_clicked()
+{
+    if (ui->lineEdit_FilePath->text().isEmpty()) {
+        qDebug()<<"文件为空";
+        return;
+    }
+
+    QFileInfo FileIno(NewFilePath);
+    QString WorkPath = FileIno.absoluteFilePath();//获取文件路径
+
+    QString Prog = FileIno.fileName();//获取
+
+
+//    QString NewFilepath = ""+"";
+    QString Env1 = ui->lineEdit_EnvPath1->text();
+    QString Env2 = ui->lineEdit_EnvPath2->text();
+
+//    Env1.replace()
+
+//  设置环境变量
+    QProcessEnvironment EnvAll = QProcessEnvironment::systemEnvironment();
+    EnvAll.insert("PATH",Env1+EnvAll.value("PATH"));
+    EnvAll.insert("PATH",Env2+EnvAll.value("PATH"));
+
+    CMD.setProcessEnvironment(EnvAll);
+    CMD.setProgram("C:/Windows/System32/cmd.exe");
+
+
+//    CMD.setArguments();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,19 @@
 #include <QProcess>
 #include <handleapi.h>
 #include <QShortcut>
+#include <qstring.h>
+
+#include<QProcess>
+
+#include<QDebug>
+
+#include<QFile>
+#include<QFileDialog>
+#include<QFileInfo>
+#include<QDir>
+
+#include<QTime>
+#include<QDate>
 
 namespace Ui {
 class MainWindow;
@@ -18,10 +31,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    QString CmdArg;
+    void LoaderCmdArg();
+    void CmdWrite(QString CmdWrVal);
+
+    QString       FilePath;
+    QString       NewFilePath;
+    QProcess    CMD;
+
 private:
     Ui::MainWindow *ui;
     QProcess *process;
     QShortcut *key;
+
 
 public slots:
     void write_cmd();
@@ -29,6 +51,15 @@ public slots:
     void finished_process();
     void error_process();
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_lineEdit_2_textEdited(const QString &arg1);
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_pushButton_PackFile_clicked();
+    void on_pushButton_OpenFile_clicked();
+    void on_pushButton_Pack_clicked();
 };
 
 #endif // MAINWINDOW_H
